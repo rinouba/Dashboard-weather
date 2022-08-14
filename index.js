eac0bc17ae7bd6b6739971124f4f4c18bb86e797
osama <012oO345678932015>
2022-08-14 18:58:01 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function myFunction(x) {
     }
   }
   
-  var x = window.matchMedia("(max-width: 1450px)")
+  var x = window.matchMedia("(max-width: 1200px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes
 
